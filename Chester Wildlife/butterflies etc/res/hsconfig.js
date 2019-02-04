e7ffbd89673794a58ae753f1e90f7bb925b2045f
chrisbrankin <chrisbrankin@hotmail.com>
2019-02-04 10:23:53 +0000

    recovering

--- Chester Wildlife/butterflies etc/res/hsconfig.js
+++ Chester Wildlife/butterflies etc/res/hsconfig.js
@@ -11,7 +11,7 @@ if (autoload) document.getElementById(autoload[1]).focus();
 });
 hs.graphicsDir = '../res/graphics/';
 hsp.stylesFile = '../res/common.css';
-hs.marginBottom = 45;
+hs.marginBottom = 15;
 hs.marginTop = 15;
 hs.marginLeft = 15;
 hs.marginRight = 15;
@@ -20,25 +20,25 @@ hs.dragByHeading = false;
 hs.preserveContent = false;
 hs.outlineType = '';
 hs.lang.loadingTitle = 'Click to cancel';
-hs.lang.previousTitle = 'Previous (left arrow)';
+hs.lang.previousTitle = 'Previous (left arrow key)';
 hs.lang.playTitle = 'Play slideshow (spacebar)';
 hs.lang.pauseTitle = 'Pause slideshow (spacebar)';
-hs.lang.nextTitle = 'Next (right arrow)';
+hs.lang.nextTitle = 'Next (right arrow key)';
 hs.lang.moveTitle = 'Click and drag to move';
 hs.lang.fullExpandTitle = 'Expand to actual size (up arrow)';
-hs.lang.closeTitle = 'Close (ESC)';
+hs.lang.closeTitle = 'Close (ESC or Enter)';
 hs.lang.previousText = 'Prev';
 hs.lang.playText = 'Play';
 hs.lang.pauseText = 'Pause';
 hs.lang.nextText = 'Next';
 hs.lang.fullExpandText = '1:1';
 hs.lang.closeText = 'Close';
-hs.lang.loadingText = 'Loading &hellip;';
-hs.lang.restoreTitle = 'Click for next';
+hs.lang.loadingText = 'Loading...';
+hs.lang.restoreTitle = 'Click to close';
 hs.align = 'center';
 hs.showCredits = false;
 hs.objectLoadTime = 'after';
-hs.dimmingOpacity = 0.85;
+hs.dimmingOpacity = 1.0;
 hs.dimmingDuration = 100;
 hs.allowMultipleInstances = false;
 hs.allowWidthReduction = true;
@@ -49,24 +49,25 @@ hs.restoreDuration = 500;
 hs.transitionDuration = 500;
 hs.easing = 'easeInBack';
 hs.easingClose = 'easeOutBack';
-hs.restoreCursor = null;
-hs.Expander.prototype.onImageClick = function() {
-	this.slideshow.pause();
-	return hs.next();
-}
 hs.onKeyDown = function(sender, e) {
 	if(e.keyCode == 13 || e.keyCode == 40 || e.keyCode == 70) return false;
-	if(e.keyCode == 32) return false;
 	if(e.keyCode == 38) return false;
 }
 hs.addSlideshow({
+	interval: 5000,
 	repeat: false,
 	overlayOptions: {
-		className: 'text-controls',
-		position: 'bottom center',
-		relativeTo: 'viewport',
-		offsetY: -10,
-		offsetX: 0
+		opacity: 0.7,
+		position: 'top right',
+		offsetY: 0,
+		offsetX: 0,
+		hideOnMouseOut: true
 	},
+	fixedControls: false,
 	useControls: true
 });
+hs.Expander.prototype.onAfterExpand = function() {
+	if (this.contentType == 'html') {
+		this.slideshow.pause();
+	}
+};
